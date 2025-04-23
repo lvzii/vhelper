28e0bcbc8a8436a808eb9ace4421c667d6793bbc
lvzi <zjs20001205@gmail.com>
2025-04-23 11:07:48 +0800

    feat: get module name

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+import re
+
+from setuptools import setup
+
+
+def get_version():
+    with open(os.path.join("src", "vhelper", "__init__.py"), "r", encoding="utf-8") as f:
+        file_content = f.read()
+        pattern = r"{}\W*=\W*\'([^\"]+)\'".format("__version__")
+        (version,) = re.findall(pattern, file_content)
+        return version
+
+
+_deps = ["nlpertools"]
+
+
+def main():
+    setup(
+        # https://juejin.cn/post/7369349560421040128
+        install_requires=_deps,
+        extras_require={
+            "none": [],
+        },
+        version=get_version(),
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/vhelper/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.0.1"
+
+from .func_analysis import *
+from .utils import *
